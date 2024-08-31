52ef8f224702c551f2ee7866a4c4b50a922b48ed
Krishna Kalakuntla <krishna.k15896@gmail.com>
2024-08-31 14:33:38 -0400

    Added security group file

---
+++ security-groups.tf
@@ -0,0 +1,8 @@
+module "web_server_sg" {
+  source              =  "app.terraform.io/training_tfc/security-group/aws"
+  version             = "4.8.0"
+  name                = "http-traffic-${var.environment}"
+  description         = "Security group for ${var.environment} with HTTP ports open within VPC"
+  vpc_id              = module.vpc.vpc_id
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
